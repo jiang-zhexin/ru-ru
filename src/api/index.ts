ae957f1fb4ec092fb4e11a116601f4a231e7b669
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2026-07-05 20:45:13 +0800

    feat: cache api response

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import v1 from "@/api/v1/index.ts";
 import { env } from "cloudflare:workers";
 import { Hono } from "hono";
+import { cache } from "hono/cache";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 
@@ -9,6 +10,13 @@ const api = new Hono();
 if (env.DEV) api.use(logger());
 
 api.use(prettyJSON({ force: env.DEV ? true : false }));
+
+api.use(
+  cache({
+    cacheName: "v1",
+    cacheControl: "public,max-age=86400",
+  }),
+);
 
 api.route("/v1", v1);
 
